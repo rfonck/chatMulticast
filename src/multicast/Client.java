47b29370cd7cd6c1c236ee17047741a1fa0cba58
rfonck <rfonck@outlook.fr>
2020-12-02 21:39:24 +0100

    Ajout des fichiers

---
+++ src/multicast/Client.java
@@ -0,0 +1,71 @@
+package multicast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.net.*;
+
+
+
+public class Client {
+
+	public static void main(String[] args) throws IOException {
+		/**
+		 * Affichage de l'erreur si l'utilisateur oublie d'entrer l'adresse du groupe ou du port 
+		 */
+	    if (args.length != 2) {
+	      System.out.println("Usage: java EchoClient <GroupAdress> <GroupPort>");
+	      System.exit(1);
+	    }
+
+	    /** Set the group IP address */
+	    InetAddress groupAddr = InetAddress.getByName(args[0]); 
+	    Integer groupPort = Integer.parseInt(args[1]); 
+	    
+	    /** Create a multicast socket**/
+	    MulticastSocket s = new MulticastSocket(groupPort); 
+	    
+	    /** Multisocket joins the group */
+	    s.joinGroup(groupAddr);
+	    
+	    /**
+	     * Creation of the buffered reader 
+	     */
+	    BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter your name: "); 
+        /**
+         * Input the name of the user
+         */
+        String name = stdIn.readLine();
+
+        /**
+         * Creation of the thread for reading messages
+         */
+	    ThreadClientReception ct = new ThreadClientReception(s, name);
+	    ct.start();
+	    
+	    /**
+	     * msg will contain the message if the user wants to send a message
+	     */
+	    String msg;
+	    
+	    while(true) {
+	    	/**
+	    	 * Set of msg when the user enters a message 
+	    	 */
+	    	msg = name + " a ecrit : \n	" + stdIn.readLine() + "\na :" + new Date().toString() + "\n";
+	    	/**
+	    	 * Datagram packet creation that will then be sent to all of the members of the group
+	    	 */
+	    	DatagramPacket hi = new DatagramPacket(msg.getBytes(), msg.length(), groupAddr, groupPort);
+	    	s.send(hi);
+	    }
+	    
+	  }
+	
+}
---
+++ src/multicast/ThreadClientReception.java
@@ -0,0 +1,65 @@
+package multicast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.MulticastSocket;
+
+public class ThreadClientReception extends Thread {
+	
+	private MulticastSocket s;
+	private String name;
+	
+	/**
+	 * 
+	 * ThreadClientReception constructor
+	 */
+	public ThreadClientReception(MulticastSocket s, String name) {
+		super();
+		this.s = s;
+		this.name = name;
+	}
+
+	public MulticastSocket getS() {
+		return s;
+	}
+
+	public void setS(MulticastSocket s) {
+		this.s = s;
+	}
+
+	@Override
+	public void run() {
+		
+		while(true) {
+			byte[] buf = new byte[1000];
+			/**
+			 * 
+			 * Datagram Packet creation that will receive the data : recv
+			 */
+			DatagramPacket recv = new  DatagramPacket(buf, buf.length);
+			try {
+				/**
+				 * Data reception into recv trough s (s being the multicast socket that joined the group in the main of Client)
+				 */
+				s.receive(recv);
+				/**
+				 * Reads the message entered into recv
+				 */
+				String message = new String(recv.getData(), 0, recv.getLength());
+				/**
+				 * If it is the ThreadClientReception of the user sending the message, it will not be sent to the user
+				 */
+				if(!message.startsWith(this.name)) {
+					System.out.println(message);
+				}
+			
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
